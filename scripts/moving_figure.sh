80a4b064e2ba839672307ebcfddc92ee59f2779a
andriy8075 <andriy8075@gmail.com>
2025-04-01 12:16:30 +0300

    debug moving script

--- scripts/moving_figure.sh
+++ scripts/moving_figure.sh
@@ -2,11 +2,19 @@
 curl -X POST http://localhost:17000 -d "white"
 curl -X POST http://localhost:17000 -d "figure 0.5 0.5"
 curl -X POST http://localhost:17000 -d "update"
+sleepTime = 0.1
 
-for i in {1..10}; do
-    x=$(echo "0.1 * $i" | bc)
-    y=$(echo "0.1 * $i" | bc)
-    curl -X POST http://localhost:17000 -d "move $x $y"
+for i in {1..5}; do
+    curl -X POST http://localhost:17000 -d "move 0.3 0"
     curl -X POST http://localhost:17000 -d "update"
-    sleep 1
+    sleep sleepTime
+    curl -X POST http://localhost:17000 -d "move 0 0.3"
+    curl -X POST http://localhost:17000 -d "update"
+    sleep sleepTime
+    curl -X POST http://localhost:17000 -d "move -0.3 0"
+    curl -X POST http://localhost:17000 -d "update"
+    sleep sleepTime
+    curl -X POST http://localhost:17000 -d "move 0 -0.3"
+    curl -X   POST http://localhost:17000 -d "update"
+    sleep sleepTime
 done
